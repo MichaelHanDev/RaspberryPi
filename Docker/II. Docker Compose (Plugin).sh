bec6f0ef7872ec3e0eac484ab9cd5844e6d702a8
MichaelHanDev <135553104+MichaelHanDev@users.noreply.github.com>
2024-03-12 15:00:34 -0500

    Update II. Docker Compose (Plugin).sh

--- Docker/II. Docker Compose (Plugin).sh
+++ Docker/II. Docker Compose (Plugin).sh
@@ -2,6 +2,11 @@
  sudo apt-get update
  sudo apt-get install docker-compose-plugin
  docker compose version
+ sudo apt update
+ sudp apt full-upgrade
+ sudo apt autoremove
+ sudo reboot
+ 
 #Update Docker Compose
- sudo apt-get update
- sudo apt-get install docker-compose-plugin
+#sudo apt-get update
+#sudo apt-get install docker-compose-plugin
