948406b3174606d8bc8daee64a47ccc18849fd59
MichaelHanDev <135553104+MichaelHanDev@users.noreply.github.com>
2024-03-12 14:56:50 -0500

    Update I. Docker apt Repo (Docker Engine).sh
    Formatting Revisions and Shell Script Fixes

--- Docker/I. Docker apt Repo (Docker Engine).sh
+++ Docker/I. Docker apt Repo (Docker Engine).sh
@@ -7,11 +7,16 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Set up Docker's APT repository:
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/raspbian \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
+
 # Install the Docker packages.
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 #Verify that the installation is successful by running the hello-world image:
 sudo docker run hello-world
+sudo apt update
+sudo apt full-upgrade
+audo reboot
