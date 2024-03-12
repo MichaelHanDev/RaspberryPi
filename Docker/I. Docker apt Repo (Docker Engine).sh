49b7a155b9ccbb30f8d701f51e8369b68d80edb6
MichaelHanDev <135553104+MichaelHanDev@users.noreply.github.com>
2024-03-12 14:49:04 -0500

    Docker
    Installation scripts for Docker

---
+++ Docker/I. Docker apt Repo (Docker Engine).sh
@@ -0,0 +1,17 @@
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/raspbian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Set up Docker's APT repository:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/raspbian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+# Install the Docker packages.
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+#Verify that the installation is successful by running the hello-world image:
+sudo docker run hello-world
---
+++ Docker/II. Docker Compose (Plugin).sh
@@ -0,0 +1,7 @@
+#Install the Docker Compose plugin
+ sudo apt-get update
+ sudo apt-get install docker-compose-plugin
+ docker compose version
+#Update Docker Compose
+ sudo apt-get update
+ sudo apt-get install docker-compose-plugin
